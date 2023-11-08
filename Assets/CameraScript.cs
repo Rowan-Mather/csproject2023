ea3f356e8c3a62fcdeb1d9233e4aee3905b9e61d
Rowan Mather <rowan.m.mather@gmail.com>
2023-11-08 17:18:08 +0000

    some gyro

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -12,95 +12,86 @@ public class CameraScript : MonoBehaviour
 {
     private AttitudeSensor _attitudeSensor;
 
-    void Start() {
-        //InputSystem.EnableDevice(Gyroscope.current);
-        //InputSystem.EnableDevice(Accelerometer.current);
-        
-        Debug.Log("hi");
-        InputSystem.EnableDevice(AttitudeSensor.current);
-        Debug.Log("im dying");
-        //InputSystem.EnableDevice(GravitySensor.current);
+    void Start() {}
 
-        _attitudeSensor = InputSystem.GetDevice<AndroidRotationVector>();
-        //if (_attitudeSensor == null)
-        //    _attitudeSensor = InputSystem.GetDevice<AndroidGameRotationVector>();
-
-        if (_attitudeSensor != null)
-            InputSystem.EnableDevice(_attitudeSensor);
-        else Debug.Log("i hate my life");
-    }
-     void Update() {
-        //Vector3 angularVelocity = Gyroscope.current.angularVelocity.ReadValue();
-        //Vector3 acceleration = Accelerometer.current.acceleration.ReadValue();
-        //Vector3 attitude = AttitudeSensor.current.attitude.ReadValue(); // ReadValue() returns a Quaternion
-        //Vector3 gravity = GravitySensor.current.gravity.ReadValue();
-
-        // transform.rotation = _attitudeSensor.attitude.ReadValue();
-        
-        //  AttitudeSensor.current.attitude.ReadValue();
-        
-
-        /*
-        text.text = $"Angular Velocity\nX={angularVelocity.x:#0.00} Y={angularVelocity.y:#0.00} Z={angularVelocity.z:#0.00}\n\n" +
-                        $"Acceleration\nX={acceleration.x:#0.00} Y={acceleration.y:#0.00} Z={acceleration.z:#0.00}\n\n" +
-                            $"Attitude\nX={attitude.x:#0.00} Y={attitude.y:#0.00} Z={attitude.z:#0.00}\n\n" +
-                             $"Gravity\nX={gravity.x:#0.00} Y={gravity.y:#0.00} Z={gravity.z:#0.00}";
-        */
-    }
-    /*
-    public UserLocationScript userLocation;
-    private float speed = 100;
-    public float sensitivity = 5.0f;
-    private AttitudeSensor _attitudeSensor;
-    private IEnumerator Start()
+    private void Update()
     {
-        _attitudeSensor = InputSystem.GetDevice<AndroidRotationVector>();
-        if (_attitudeSensor == null)
-            _attitudeSensor = InputSystem.GetDevice<AndroidGameRotationVector>();
+        UpdateGyro();
+    }    
 
-        if (_attitudeSensor != null)
-            InputSystem.EnableDevice(_attitudeSensor);
-        
-        //https://issuetracker.unity3d.com/issues/android-gyroscope-functionality-doesnt-respond-in-the-player-when-using-a-tablet
+    private void UpdateGyro() {
+        var gyro = GetRemoteDevice<Gyroscope>();
+        var attitude = GetRemoteDevice<AttitudeSensor>();
+        var gravity = GetRemoteDevice<GravitySensor>();
+        var acceleration = GetRemoteDevice<LinearAccelerationSensor>();
+
+        // Enable gyro from remote, if needed.
+        EnableDeviceIfNeeded(gyro);
+        EnableDeviceIfNeeded(attitude);
+        EnableDeviceIfNeeded(gravity);
+        EnableDeviceIfNeeded(acceleration);
+
+        string text;
+        if (gyro == null && attitude == null && gravity == null && acceleration == null)
+        {
+            text = "No remote gyro found.";
+        }
+        else
+        {
+            string gyroText = null;
+            string attitudeText = null;
+            string gravityText = null;
+            string accelerationText = null;
+
+            if (gyro != null)
+            {
+                var rotation = gyro.angularVelocity.ReadValue();
+                gyroText = $"Rotation: x={rotation.x} y={rotation.y} z={rotation.z}";
+
+                // Update rotation of cube.
+                //m_Rotation += rotation;
+                //rotatingCube.localEulerAngles = m_Rotation;
+            }
+
+            if (attitude != null)
+            {
+                var attitudeValue = attitude.attitude.ReadValue();
+                attitudeText = $"Attitude: x={attitudeValue.x} y={attitudeValue.y} z={attitudeValue.z} w={attitudeValue.w}";
+            }
+
+            if (gravity != null)
+            {
+                var gravityValue = gravity.gravity.ReadValue();
+                gravityText = $"Gravity: x={gravityValue.x} y={gravityValue.y} z={gravityValue.z}";
+            }
+
+            if (acceleration != null)
+            {
+                var accelerationValue = acceleration.acceleration.ReadValue();
+                accelerationText = $"Acceleration: x={accelerationValue.x} y={accelerationValue.y} z={accelerationValue.z}";
+            }
+
+            text = string.Join("\n", gyroText, attitudeText, gravityText, accelerationText);
+            Debug.Log(text);
+        }
+
+        //gyroInputText.text = text;
     }
 
-    private void ApplyGyroRotation()
+   private static void EnableDeviceIfNeeded(InputDevice device)
     {
-        //Quaternion gyroAttitude = Input.gyro.attitude;
-        Quaternion gyroAttitude = Quaternion.identity;
-        if (_attitudeSensor != null)
-            gyroAttitude = _attitudeSensor.attitude.ReadValue();
-    } 
+        if (device != null && !device.enabled)
+            InputSystem.EnableDevice(device);
+    }
 
-    //void Start() {
-    //    Input.gyro.enabled = false;
-    //    Input.gyro.enabled = true;
-    //}
-    void Update()
+    // Make sure we're not thrown off track by locally having sensors on the device. Instead
+    // explicitly grab the remote ones.
+    private static TDevice GetRemoteDevice<TDevice>() 
+        where TDevice : InputDevice
     {
-
-        if (_attitudeSensor != null)
-            transform.rotation = _attitudeSensor.attitude.ReadValue();
-
-        //transform.rotation = userLocation.SceneRotation;
-
-
-        //userLocation.SceneRotation;
-        
-        // Move the camera forward, backward, left, and right
-        //transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        //transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-
-        //float a = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        //float b = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-
-        // Rotate the camera based on the mouse movement
-        //float mouseX = Input.GetAxis("Mouse X");
-        //float mouseY = Input.GetAxis("Mouse Y");
-        //transform.eulerAngles += new Vector3(-mouseY * sensitivity, mouseX * sensitivity, 0);
-        
-        //transform.eulerAngles += new Vector3(-a, b, 0);
-        //Quaternion.Euler(-v, h, 0);
-        
-    }*/
+        foreach (var device in InputSystem.devices)
+            if (device.remote && device is TDevice deviceOfType)
+                return deviceOfType;
+        return default;
+    }
 }
